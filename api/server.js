9706da068eb50c1aac090b58074a661a4d9a0c11
mcarter78 <sjsharksftw@gmail.com>
2016-06-29 13:54:04 -0700

    adds hapi server

---
+++ api/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var Hapi = require('hapi');
+
+// Create a server with a host and port
+var server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/hello',
+    handler: function (request, reply) {
+
+        return reply('hello world');
+    }
+});
+
+// Start the server
+server.start(function(err) {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
