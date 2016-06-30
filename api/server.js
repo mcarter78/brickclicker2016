cc35570d8a381ec1eee6ebf81e45fc163288da0c
mcarter78 <sjsharksftw@gmail.com>
2016-06-29 17:30:41 -0700

    changes host port

--- api/server.js
+++ api/server.js
@@ -5,8 +5,8 @@ var Hapi = require('hapi');
 // Create a server with a host and port
 var server = new Hapi.Server();
 server.connection({
-    host: 'localhost',
-    port: 8000
+    // host: 'localhost',
+    port: 3000
 });
 
 server.register(require('inert'), function() {
